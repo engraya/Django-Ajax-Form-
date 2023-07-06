3d3cbbeb218ecaf540254cd83a28898e683da204
Engraya <ahmadyakubuaya@gmail.com>
2023-07-06 12:37:32 +0100

    Final Commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('index', views.index, name='index'),
+    path('', views.home, name="home"),
+    path('newPost', views.createPost, name="newPost")
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+from .models import Caption
 
 # Create your views here.
 
 
 def index(request):
     return render(request, 'myapp/base.html')
+
+
+def home(request):
+    captions = Caption.objects.all().order_by('-id')
+    context = {'captions' : captions}
+    return render(request, 'myapp/home.html', context)
+
+
+def createPost(request):
+    if request.POST.get('action') == 'newPost':
+        title  = request.POST.get('title')
+        description  = request.POST.get('description')
+        image  = request.FILES.get('image')
+
+        Caption.objects.create(
+            title=title,
+            description=description,
+            image=image
+        )
+
+    return render(request, 'myapp/newPost.html')
